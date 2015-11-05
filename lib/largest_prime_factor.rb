2cb503709ea6d4dcdfba9471996b0f963e5984b6
mikeappell <mikeappell@gmail.com>
2015-11-05 17:09:31 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,13 @@
-# Enter your procedural solution here!
+require 'prime'
+
+def largest_prime_factor(num)
+  largest, guess = 0, 2
+  while guess <= num do
+    if (num % guess == 0 && Prime::prime?(guess))
+      largest = guess
+      num /= guess
+    end
+    guess += 1
+  end
+  largest
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,19 @@
-# Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    largest, guess = 0, 2
+    while guess <= @num do
+      if (@num % guess == 0 && Prime::prime?(guess))
+        largest = guess
+        @num /= guess
+      end
+      guess += 1
+    end
+    largest
+  end
+end
